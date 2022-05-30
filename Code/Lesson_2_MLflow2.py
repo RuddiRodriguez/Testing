72d26ee538c1e3e71f16918c04a96ba869e9f6bc
Ruddi <rudiscu@gmail.com>
2022-05-30 10:26:21 +0200

    Homewrok2_start

--- Code/Lesson_2_MLflow2.py
+++ Code/Lesson_2_MLflow2.py
@@ -12,6 +12,10 @@ import pickle
 import os
 import mlflow
 from pathlib import Path
+from xgboost import XGBRegressor
+import numpy as np
+import optuna
+from sklearn.model_selection import StratifiedKFold,cross_val_score
 import warnings
 warnings.filterwarnings("ignore")
 #%%
@@ -70,35 +74,59 @@ def pre_processing(df):
     return df[['PUlocationID', 'DOlocationID', 'duration']]
 
 #%%
-def main():
+def objective(trial, X, y):
+    
     mlflow.set_tracking_uri(r"file\\\D:\Python\Projects\NL_Automated_Reports\Testing\mlflow")
-    run_name = mlflow.set_experiment("mlflow_experiment2")
-    dir_name = Path(__file__).parents[1]
+    run_name = mlflow.set_experiment("mlflow_experiment_optuna1")
+    
     with mlflow.start_run(run_name=run_name,nested=True):
-        mlflow.set_tags({"model": "linear", "encode": "one_hot"})
-        mlflow.log_param("model", "linear")
-        mlflow.log_artifact(os.path.join(dir_name, 'Model'))
-        df = getting_data(dir_name)
-        df = new_features(df)
+        max_depth = trial.suggest_int('max_depth', 2, 500) 
+        reg_alpha = trial.suggest_uniform('reg_alpha', 0.0, 1.0) 
+        learning_rate = trial.suggest_loguniform('learning_rate', 1e-3, 1e-1) 
 
-        df_january = df[df.pickup_datetime.dt.month == 1]
-        df_february = df[df.pickup_datetime.dt.month == 2]
+        params = {
+            'max_depth': max_depth,
+            'reg_alpha': reg_alpha,
+            'learning_rate': learning_rate} 
 
-        cat_features = ['PUlocationID', 'DOlocationID']
 
-        # Question 4: Dimensionality after OHE
-        df_january = pre_processing(df_january)
-        X_train, y_train = get_x_y(df_january)
-        X_train_scaled = feature_transformation(X_train, cat_features, dir_name)
+        cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1121218)
+        regressor_obj =XGBRegressor()
+        score = cross_val_score(regressor_obj.set_params(**params), X, y, n_jobs=-1, cv=5,scoring='neg_mean_absolute_error')
+        mlflow.log_metric("MAE", -score.mean())
+    return np.mean((-1)*score)
+
+#%%
+def main():
+    
+    dir_name = Path(__file__).parents[1]
+    
+    
+    df = getting_data(dir_name)
+    df = new_features(df)
+
+    df_january = df[df.pickup_datetime.dt.month == 1]
+    df_february = df[df.pickup_datetime.dt.month == 2]
+
+    cat_features = ['PUlocationID', 'DOlocationID']
+
+    # Question 4: Dimensionality after OHE
+    df_january = pre_processing(df_january)
+    X_train, y_train = get_x_y(df_january)
+    X_train_scaled = feature_transformation(X_train, cat_features, dir_name)
+    
+    study = optuna.create_study(direction="minimize", study_name="xgboost_optuna")
+    func = lambda trial: objective(trial, X_train_scaled, y_train)
+    study.optimize(func, n_trials=50) 
         
-        # Question 5 : RMSE of the trained model
+    """ # Question 5 : RMSE of the trained model
         regressor = LinearRegression()
         model = regressor.fit(X_train_scaled, y_train)
         predictions = model.predict(X_train_scaled)
         
         rmse = sqrt(mean_squared_error(y_train, predictions))
         mlflow.log_metric("RMSE", rmse)
-
+    """
 
     """ df_february = pre_processing(df_february)
     X_valid, y_valid = get_x_y(df_february)
